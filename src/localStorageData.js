5a4c54b9f8406ff2288e5a0f820552aa4fa6f7a3
Luis Puente <puente.luism@gmail.com>
2020-04-07 17:13:05 -0500

    Local storage

--- src/localStorageData.js
+++ src/localStorageData.js
@@ -1,5 +1,7 @@
 const currentIdCount = localStorage.getItem('id-count');
 const currentList = localStorage.getItem('current-list');
+const projectId = localStorage.getItem('project-id');
+const projectList = localStorage.getItem('project-list');
 
 const getStorage = () => {
   let id = 0;
@@ -10,10 +12,26 @@ const getStorage = () => {
   }
   return [id, list];
 };
-
 const setStorage = (idCount, list) => {
   localStorage['id-count'] = idCount;
   localStorage['current-list'] = JSON.stringify(list);
 };
 
-export { getStorage, setStorage };
+const setProject = (id, list) => {
+  localStorage['project-id'] = id;
+  localStorage['project-list'] = JSON.stringify(list);
+};
+
+const getProject = () => {
+  let id = 0;
+  let list = {};
+  if (projectId !== undefined) {
+    id = Number(projectId);
+    list = JSON.parse(projectList);
+  }
+  return [id, list];
+};
+
+export {
+  getStorage, setStorage, setProject, getProject,
+};
